c159567119522a70b1251cd403817badf515425f
bm-213790 <bm-213790@hs-weingarten.de>
2023-10-13 15:33:08 +0200

    imports

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,22 @@ import HelloWorld from './components/HelloWorld.vue';
 import TheWelcome from './components/TheWelcome.vue';
 import Header from './components/Header.vue';
 import DisplayBarcodesList from './components/DisplayBarcodesList.vue';
-import BarcodeScanner from './components/BarcodeScanner.vue';
+
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+      <Header></Header>
     </div>
   </header>
 
   <main>
+    <BarcodeScanner />
+    <DisplayBarcodesList />
     <TheWelcome />
   </main>
 </template>
